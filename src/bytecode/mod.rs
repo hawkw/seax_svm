e24118051d7ff88182410b62ec2669faa325e9dc
Hawk Weisman <hi@hawkweisman.me>
2015-07-20 14:23:49 -0400

    shortened some lengthy stuff

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -251,12 +251,8 @@ impl<'a, R> Decoder<'a, R> where R: Read {
             Ok(0)       => Err(String::from(
                 "Reached end of source unexpectedly while decoding cons cell")),
             Ok(_)       => self.next_cell()
-                               .and_then(|car|
-                                   (car, try!(self.next_cell()))
-                                )
-                               .and_then(|(car, cdr)|
-                                    Some(box list!(car,cdr))
-                                )
+                               .and_then(|car| (car, try!(self.next_cell())) )
+                               .and_then(|(car, cdr)| Some(box list!(car,cdr)) )
             Err(why)    => Err(String::from(why.description()))
         }
 
