c3e5aef5163187023886e5d05b569697887672ee
Hawk Weisman <hi@hawkweisman.me>
2015-07-22 15:20:54 -0400

    Add tests for instruction encoding/decoding

--- src/bytecode/tests.rs
+++ src/bytecode/tests.rs
@@ -1,11 +1,22 @@
-use super::Decoder;
-use super::Encode;
-use ::cell::Atom;
-use ::cell::SVMCell;
+use super::{Encode,Decoder};
+use ::cell::{Atom,Inst,SVMCell};
 
 use std::io::Cursor;
 
 use quickcheck::quickcheck;
+
+macro_rules! impl_encode_test {
+    ($name:ident, $it:expr) => {
+        #[test]
+        fn $name () {
+            let cell = $it;
+            let encoded = cell.emit();
+            let decoded = Decoder::new(&mut Cursor::new(encoded)).next_cell();
+            assert_eq!(Ok(Some(cell)), decoded)
+        }
+
+    }
+}
 
 #[test]
 fn prop_encode_float () {
@@ -51,10 +62,119 @@ fn prop_encode_char () {
     quickcheck(prop as fn(char) -> bool);
 }
 
-#[test]
-fn test_encode_uint_zero () {
-    let cell = SVMCell::AtomCell(Atom::UInt(0));
-    let encoded = cell.emit();
-    let decoded = Decoder::new(&mut Cursor::new(encoded)).next_cell();
-    assert_eq!(decoded, Ok(Some(cell)))
-}
+impl_encode_test!(
+    test_encode_uint_zero,
+    SVMCell::AtomCell(Atom::UInt(0))
+);
+impl_encode_test!(
+    test_encode_inst_nil,
+    SVMCell::InstCell(Inst::NIL)
+);
+impl_encode_test!(
+    test_encode_inst_ld,
+    SVMCell::InstCell(Inst::LD)
+);
+impl_encode_test!(
+    test_encode_inst_ldf,
+    SVMCell::InstCell(Inst::LDF)
+);
+impl_encode_test!(
+    test_encode_inst_ap,
+    SVMCell::InstCell(Inst::AP)
+);
+impl_encode_test!(
+    test_encode_inst_apcc,
+    SVMCell::InstCell(Inst::APCC)
+);
+impl_encode_test!(
+    test_encode_inst_join,
+    SVMCell::InstCell(Inst::JOIN)
+);
+impl_encode_test!(
+    test_encode_inst_rap,
+    SVMCell::InstCell(Inst::RAP)
+);
+impl_encode_test!(
+    test_encode_inst_ret,
+    SVMCell::InstCell(Inst::RET)
+);
+impl_encode_test!(
+    test_encode_inst_dum,
+    SVMCell::InstCell(Inst::DUM)
+);
+impl_encode_test!(
+    test_encode_inst_sel,
+    SVMCell::InstCell(Inst::SEL)
+);
+impl_encode_test!(
+    test_encode_inst_add,
+    SVMCell::InstCell(Inst::ADD)
+);
+impl_encode_test!(
+    test_encode_inst_sub,
+    SVMCell::InstCell(Inst::SUB)
+);
+impl_encode_test!(
+    test_encode_inst_mul,
+    SVMCell::InstCell(Inst::MUL)
+);
+impl_encode_test!(
+    test_encode_inst_div,
+    SVMCell::InstCell(Inst::DIV)
+);
+impl_encode_test!(
+    test_encode_inst_mod,
+    SVMCell::InstCell(Inst::MOD)
+);
+impl_encode_test!(
+    test_encode_inst_fdiv,
+    SVMCell::InstCell(Inst::FDIV)
+);
+impl_encode_test!(
+    test_encode_inst_eq,
+    SVMCell::InstCell(Inst::EQ)
+);
+impl_encode_test!(
+    test_encode_inst_gt,
+    SVMCell::InstCell(Inst::GT)
+);
+impl_encode_test!(
+    test_encode_inst_gte,
+    SVMCell::InstCell(Inst::GTE)
+);
+impl_encode_test!(
+    test_encode_inst_lt,
+    SVMCell::InstCell(Inst::LT)
+);
+impl_encode_test!(
+    test_encode_inst_lte,
+    SVMCell::InstCell(Inst::LTE)
+);
+impl_encode_test!(
+    test_encode_inst_atom,
+    SVMCell::InstCell(Inst::ATOM)
+);
+impl_encode_test!(
+    test_encode_inst_null,
+    SVMCell::InstCell(Inst::NULL)
+);
+impl_encode_test!(
+    test_encode_inst_cons,
+    SVMCell::InstCell(Inst::CONS)
+);
+impl_encode_test!(
+    test_encode_inst_car,
+    SVMCell::InstCell(Inst::CAR)
+);
+impl_encode_test!(
+    test_encode_inst_cdr,
+    SVMCell::InstCell(Inst::CDR)
+);
+impl_encode_test!(
+    test_encode_inst_ldc,
+    SVMCell::InstCell(Inst::LDC)
+);
+impl_encode_test!(
+    test_encode_inst_stop,
+    SVMCell::InstCell(Inst::STOP)
+);
